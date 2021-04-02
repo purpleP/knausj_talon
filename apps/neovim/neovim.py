8c4659e19156f15afb18bb413dd20ef717057378
Michael <warrior2031@gmail.com>
2021-04-02 10:53:44 +0300

    wip

---
+++ apps/neovim/neovim.py
@@ -0,0 +1,39 @@
+import pynvim as vi
+
+from talon import Context, Module, actions
+
+
+mod = Module()
+
+@mod.action_class
+class Actions:
+    def vim_paste(key):
+        pass
+
+insert_mode_context = Context()
+ctx.matches = r'''
+app: neovim
+tag: user.vim_insert
+'''
+
+normal_mode_context = Context()
+ctx.matches = r'''
+app: neovim
+tag: user.vim_normal
+'''
+
+
+
+@insert_mode_context.action_class
+class Actions:
+    def vim_paste(key):
+        actions.key("ctrl-r")
+        actions.next(key) # performs the next action implementation in the chai
+
+@normal_mode_context.action_class
+class Actions:
+    def vim_paste(key):
+        actions.key('"')
+        actions.key(key or '"')
+        actions.key('p')
+        ctx.tags = ['user.vim_insert']
