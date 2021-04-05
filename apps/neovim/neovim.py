9593d33828082edb320a500146039585d5cde0b4
Michael <warrior2031@gmail.com>
2021-04-05 12:15:53 +0300

    wip

--- apps/neovim/neovim.py
+++ apps/neovim/neovim.py
@@ -4,26 +4,38 @@ from talon import Context, Module, actions
 
 
 mod = Module()
-nmod.apps.neovim = 'app.name: neovim'
+mod.apps.neovim = 'app.name: neovim'
 
 mod.tag('vim_insert', 'Insert mode in vim')
 mod.tag('vim_normal', 'Insert mode in vim')
 
 @mod.action_class
 class Actions:
-    def vim_paste(key: str):
-        """Some doc string"""
+    def vim_paste_before(key: str):
+        """Paste from register before the cursor"""
         pass
+    
+    def vim_paste_after(key: str):
+        """Paste from register after the cursor"""
+        pass
+    
+    def vim_delete_line():
+        """Add line before current one"""
+        actions.key('O')
+
+    def vim_line_before():
+        """Add line before current one"""
+        actions.key('O')
 
 insert_mode_context = Context()
 insert_mode_context.matches = r'''
-app: neovim
+app.name: Neovim
 tag: user.vim_insert
 '''
 
 normal_mode_context = Context()
 normal_mode_context.matches = r'''
-app: neovim
+app.name: Neovim
 tag: user.vim_normal
 '''
 
@@ -31,13 +43,18 @@ tag: user.vim_normal
 
 @insert_mode_context.action_class('self')
 class InsertModeAction:
-    def vim_paste(key: str):
+    def vim_paste_after(key: str):
         actions.key('ctrl-r')
-        actions.next(key) # performs the next action implementation in the chai
+        actions.key(key or '"') # performs the next action implementation in the chai
 
 @normal_mode_context.action_class('self')
 class NormalModeActions:
-    def vim_paste(key: str):
+    def vim_paste_before(key: str):
+        actions.key('"')
+        actions.key(key or '"')
+        actions.key('P')
+    
+    def vim_paste_after(key: str):
         actions.key('"')
         actions.key(key or '"')
         actions.key('p')
