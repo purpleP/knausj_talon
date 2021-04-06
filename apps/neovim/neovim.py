e2b709e5cd3c1fa76fcb29bc56a145deaa22fe0f
Michael <warrior2031@gmail.com>
2021-04-06 13:37:55 +0300

    wip

--- apps/neovim/neovim.py
+++ apps/neovim/neovim.py
@@ -9,18 +9,70 @@ mod.apps.neovim = 'app.name: neovim'
 mod.tag('vim_insert', 'Insert mode in vim')
 mod.tag('vim_normal', 'Insert mode in vim')
 
-counted_verbs = {
+verbs = {
     'delete': 'd',
+    'change': 'c',
+    'cut': 'x',
+    'cut back': 'X',
+    'comment': 'gc',
+    'exchange': 'cx',
+    'select': 'v',
 }
 
-text_objects = {
+line_verbs = {
+    'indent': '>',
+    'dedent': '<',
+    'select line': 'V',
+}
+
+line_motions = {
+    'up': 'k',
+    'down': 'j',
+}
+
+only_text_objects = {
+    'paragraph': 'p',
+    'quotes': "'",
+    'double quotes': '"',
+}
+
+text_objects_and_motions = {
     'word': 'w',
     'big': 'W',
     'back': 'b',
     'big back': 'B',
+    'end': 'e',
+    'big end': 'E',
 }
 
+only_motions = {
+    'up': 'k',
+    'down': 'j',
+    'left': 'h',
+    'right': 'l',
+    'search': '/',
+    'search back': '?',
+    'again': ';',
+    'come back': ',',
+    'next end of function': ']M',
+    'previous end of function': '[M',
+    'next start of function': ']m',
+    'previous start of function': '[m',
+}
 
+motions_with_letter = {
+    'untill': 't',
+    'back untill': 'T',
+    'to': 'f',
+    'back to': 'F',
+}
+
+text_object_modifiers = {
+    'a': 'a',
+    'around': 'a',
+    'inner': 'i',
+    'linewise': 'V',
+}
 
 @mod.action_class
 class Actions:
@@ -53,14 +105,49 @@ app.name: Neovim
 tag: user.vim_normal
 '''
 
-@mod.capture(rule='{self.vim_counted_verbs} [<number>] {self.vim_text_objects}')
-def count_verb_object(parts: str) -> str:
+@mod.capture(rule='[<number>] {self.vim_motions_with_letter} <user.letter>')
+def vim_count_motion_letter(parts: str) -> str:
+    """Returns action"""
     return ''.join(map(str, parts))
 
-mod.list('vim_counted_verbs', 'Vim counted verbs')
+@mod.capture(rule='[<number>] {self.vim_motions}')
+def vim_count_motion(parts: str) -> str:
+    """Returns action"""
+    return ''.join(map(str, parts))
+
+@mod.capture(rule='{self.vim_line_verbs} [<number>] {self.vim_line_motions}')
+def vim_count_verb_line_object(parts: str) -> str:
+    """Returns action"""
+    return ''.join(map(str, parts))
+
+@mod.capture(rule='{self.vim_verbs} [<number>] [{self.vim_text_object_modifiers}] ({self.vim_text_objects_and_motions} | {self.vim_text_objects} | {self.vim_motions})')
+def vim_count_verb_object(parts: str) -> str:
+    """Returns action"""
+    return ''.join(map(str, parts))
+
+mod.list('vim_verbs', 'Vim counted verbs')
+normal_mode_context.lists['self.vim_verbs'] = {**verbs}
+
 mod.list('vim_text_objects', 'Vim text objects')
-normal_mode_context.lists['self.vim_counted_verbs'] = {**counted_verbs}
-normal_mode_context.lists['self.vim_text_objects'] = {**text_objects}
+normal_mode_context.lists['self.vim_text_objects'] = {**only_text_objects}
+
+mod.list('vim_text_objects_and_motions', 'Vim text objects and motions')
+normal_mode_context.lists['self.vim_text_objects_and_motions'] = {**text_objects_and_motions}
+
+mod.list('vim_text_object_modifiers', 'Vim text object modifiers')
+normal_mode_context.lists['self.vim_text_object_modifiers'] = {**text_object_modifiers}
+
+mod.list('vim_line_verbs', 'Vim line verbs')
+normal_mode_context.lists['self.vim_line_verbs'] = {**line_verbs}
+
+mod.list('vim_line_motions', 'Vim line motions')
+normal_mode_context.lists['self.vim_line_motions'] = {**line_motions}
+
+mod.list('vim_motions', 'Vim motions')
+normal_mode_context.lists['self.vim_motions'] = {**only_motions, **text_objects_and_motions}
+
+mod.list('vim_motions_with_letter', 'Vim motions with letter')
+normal_mode_context.lists['self.vim_motions_with_letter'] = {**motions_with_letter}
 
 @insert_mode_context.action_class('self')
 class InsertModeAction:
